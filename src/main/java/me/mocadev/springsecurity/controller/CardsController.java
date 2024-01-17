d3f2867f2bbe36f51dbbe47996c353a314f1bd13
tony <chcjswo@gmail.com>
2024-01-18 00:50:22 +0900

    security 기본설정 적용

--- src/main/java/me/mocadev/springsecurity/controller/CardsController.java
+++ src/main/java/me/mocadev/springsecurity/controller/CardsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CardsController {
 
-	@GetMapping("/myCard")
+	@GetMapping("/myCards")
 	public String getCardDetail() {
 		return "card detail";
 	}
--- src/main/java/me/mocadev/springsecurity/controller/LoansController.java
+++ src/main/java/me/mocadev/springsecurity/controller/LoansController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoansController {
 
-	@GetMapping("/myLoan")
+	@GetMapping("/myLoans")
 	public String getLoanDetail() {
 		return "loan detail";
 	}
